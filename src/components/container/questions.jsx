6b284fb018909be0a1639ca88d236932bda0538d
Sem Gabriel Roig <sem.roig@gmail.com>
2023-05-24 11:15:20 -0300

    get the list of questions

--- src/components/container/questions.jsx
+++ src/components/container/questions.jsx
@@ -4,6 +4,9 @@ import { Client, Databases, ID } from 'appwrite';
 
 const Questions = () => {
 
+    const [questionRecords, setQuestionRecords] = useState([]);
+    const [count, setCount] = useState(0)
+
     const client = new Client();
     const databases = new Databases(client);
 
@@ -11,19 +14,25 @@ const Questions = () => {
         .setEndpoint('https://cloud.appwrite.io/v1')
         .setProject('646be96ac195499e31c0');
 
-    // Con esto se pueden crear registros!
-    const promise = databases.createDocument(
-        '646beb350730365f9314', '646beb48cf58113e0f44', ID.unique(), {
-            Description: "prueba"
-        }
-    );
+    // // Con esto se pueden crear registros!
+    // const promise = databases.createDocument(
+    //     '646beb350730365f9314', '646beb48cf58113e0f44', ID.unique(), {
+    //         Description: "prueba"
+    //     }
+    // );
 
-    promise.then(function (response) {
-        console.log(response); // Success
-        // setQuestions(response.documents);
-    }, function (error) {
-        console.log(error); // Failure
-    });
+    // useEffect(() => {
+
+    //     const promise = databases.listDocuments('646beb350730365f9314', '646e0add6b18024c13be');
+
+    //     promise.then(function (response) {
+    //         console.log(response); // Success
+    //         setQuestionRecords(response.documents);
+    //     }, function (error) {
+    //         console.log(error); // Failure
+    //     });
+
+    // }, [count]);
 
     // const [questions, setQuestions] = useState([]);
 
@@ -43,7 +52,17 @@ const Questions = () => {
 
     return (
         <div>
-            cosas
+            <h3>Question number {count}</h3>
+
+            <p>{ questionRecords }</p>
+
+            {questionRecords.map((pregunta) => (
+                <p key={ pregunta['$id'] }>{ pregunta['Description'] }</p>
+            ))}
+
+            <button onClick={() => setCount((count) => count + 1)}>
+                Next quesion
+            </button>
         </div>
     );
 }
