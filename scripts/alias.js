70bd0ebdfd2253c545d7c6a395c590168c9e68fe
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-05-26 19:05:23 -0300

    config root folders alias
    Signed-off-by: Matias M. Monasterio <matias_monasterio@outlook.com>

---
+++ scripts/alias.js
@@ -0,0 +1,21 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const alias = [];
+const srcPath = path.join(__dirname, "../src");
+
+fs.readdirSync(srcPath).map((dir) => {
+    const dirPath = path.join(srcPath, dir);
+  
+    if (fs.lstatSync(dirPath).isDirectory()) {
+      alias.push({
+        name: dir,
+        path: path.join(srcPath, dir),
+      });
+    }
+  });
+
+export const viteAlias = alias.map((currentAlias) => ({
+    find: currentAlias.name,
+    replacement: currentAlias.path,
+}));
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { viteAlias } from "./scripts/alias";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: viteAlias
+  }
 })
