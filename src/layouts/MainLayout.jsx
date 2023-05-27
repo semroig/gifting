9d634461266dcf9fb76859b8483d686eb12b4f8b
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-05-27 13:06:13 -0300

    set main layout styles
    Signed-off-by: Matias M. Monasterio <matias_monasterio@outlook.com>

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { Container, Box, Grid } from "@chakra-ui/react";
+
 import Navbar from "components/container/navbar";
 import Footer from "components/container/footer";
 
 export default function MainLayout() {
   return (
-    <div>
+    <Grid minH="100vh" flexDir="column" templateRows="auto 1fr auto">
         <Navbar />
-        <Outlet />
-        <Footer />
-    </div>
+
+        <Container maxW="container.lg" h="100%">
+          <Outlet />
+        </Container>
+
+        <Box mt="auto">
+          <Footer />
+        </Box>
+    </Grid>
   )
 }
