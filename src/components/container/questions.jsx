f92f8d20ec7fcf44b95c2dd2218b01a5f4cbc63b
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-05-27 10:23:22 -0300

    implement questions services and quiz actions
    Signed-off-by: Matias M. Monasterio <matias_monasterio@outlook.com>

--- src/components/container/questions.jsx
+++ src/components/container/questions.jsx
@@ -1,75 +1,41 @@
-import { React, useState, useEffect } from 'react';
-import { Client, Databases, ID } from 'appwrite';
-
+import { useState, useEffect } from 'react';
+import { questionsService } from "services";
 
 const Questions = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [question, setQuestion] = useState(null);
 
-    const [questionRecords, setQuestionRecords] = useState([]);
-    const [totalRecords, setTotalRecords] = useState(0);
-    const [count, setCount] = useState(0);
+    const handleResponse = (event) => {
+        const response = event.target.value
+        console.log({response}) // do something with response
+        getRandomQuestion()
+    }
 
-    const client = new Client();
-    const databases = new Databases(client);
+    const getRandomQuestion = async () => {
+        setIsLoading(true)
 
-    client
-        .setEndpoint('https://cloud.appwrite.io/v1')
-        .setProject('646be96ac195499e31c0');
+        questionsService.getRandomQuestion()
+            .then(setQuestion)
+            .catch(console.error)
+            .finally(() => setIsLoading(false))
+    }
 
-    // On component mount (an ONLY once), I retrieve all the questions available
     useEffect(() => {
+        getRandomQuestion()
+    }, [])
 
-        // Get the array of questions
-        const promise = databases.listDocuments('646beb350730365f9314', '646e0add6b18024c13be');
-
-        // Store the array on the questionRecords State
-        promise.then(function (response) {
-            console.log(response); // Success
-
-            // Set the states with the data retrieved
-            setQuestionRecords(response.documents);
-            setTotalRecords(response.total);
-        }, function (error) {
-            console.log(error); // Failure
-        });
-
-    }, []);
-
-    // const [questions, setQuestions] = useState([]);
-
-    // useEffect(() => {
-    //     const promise = client.subscribe(['collections.questions.documents'], (data) => {
-                
-    //     });
-
-    //     promise.then(function (response) {
-    //         console.log(response); // Success
-    //         setQuestions(response.documents);
-    //     }, function (error) {
-    //         console.log(error); // Failure
-    //     });
-
-    //   }, [questions]);
+    if(isLoading) return <p>Loading...</p>
+    if(!question) return <p>No questions</p>
 
     return (
         <div>
-            <h3>Question number {count}</h3>
-
-            {/* <p>{ questionRecords[0]['Description'] }</p> */}
-
-            {questionRecords.map(pregunta => (
-                <div key={ pregunta['$id'] }>
-                    <p>{ pregunta['Description'] }</p>
-                </div>
-            ))}
-
-            <button onClick={() => {
-                setCount((count) => count + 1);
-
-                console.log("random number for index");
-                console.log(Math.random() * totalRecords | 0);
-            }}>
-                Next question
-            </button>
+            <p> {question.Description} </p>
+            
+            <div>
+                <button value='yes' onClick={handleResponse}>Yes</button>
+                <button value="no" onClick={handleResponse}>No</button>
+                <button value="maybe" onClick={handleResponse}>Maybe</button>
+            </div>
         </div>
     );
 }
