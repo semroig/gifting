d2e82ac3d51bf3783368657cbaa342ba88c4dac2
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-05-27 13:05:58 -0300

    implement basic style to auth pages
    Signed-off-by: Matias M. Monasterio <matias_monasterio@outlook.com>

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
+import { Container, Grid, GridItem, Heading, Button, Flex } from "@chakra-ui/react";
 
 const SigInPage = () => {
-    return (
-        <div>
-            <Link to="/quiz">Sign in</Link>
-        </div>
-    );
-}
+  return (
+    <Grid minH="100vh" templateRows="1fr">
+      <Container maxW="container.lg" py={20} h="100%">
+        <Grid templateColumns="repeat(2, 1fr)" gap={20} h="100%">
+          <Flex flexDir="column">
+            <Heading mb={10}>Sign in</Heading>
+
+            formulario
+
+            <Button as={Link} to="/quiz" w="100%" mt="auto">
+              Sign in
+            </Button>
+          </Flex>
+
+          <GridItem bg="gray.200" />
+        </Grid>
+      </Container>
+    </Grid>
+  );
+};
 
 export default SigInPage;
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
+import { Container, Grid, GridItem, Heading, Button, Flex } from "@chakra-ui/react";
 
 const SignUpPage = () => {
     return (
-        <div>
-            <Link to="/quiz">Sign up</Link>
-        </div>
-    );
+        <Grid minH="100vh" templateRows="1fr">
+          <Container maxW="container.lg" py={20} h="100%">
+            <Grid templateColumns="repeat(2, 1fr)" gap={20} h="100%">
+              <Flex flexDir="column">
+                <Heading mb={10}>Sign up</Heading>
+    
+                formulario
+    
+                <Button as={Link} to="/quiz" w="100%" mt="auto">
+                  Sign up
+                </Button>
+              </Flex>
+    
+              <GridItem bg="gray.200" />
+            </Grid>
+          </Container>
+        </Grid>
+      );
 }
 
 export default SignUpPage;
