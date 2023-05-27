20293a1b07aeec84dd68647ec7ec65fbb6a2ead5
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-05-27 13:04:00 -0300

    set footer basic styles and curr year
    Signed-off-by: Matias M. Monasterio <matias_monasterio@outlook.com>

--- src/components/container/footer.jsx
+++ src/components/container/footer.jsx
@@ -1,9 +1,20 @@
-import Logo from "components/pure/logo"
+import { Container, Box, Grid, Flex, Text } from "@chakra-ui/react";
+import Logo from "components/pure/logo";
+
+const currentYear = new Date().getFullYear();
 
 export default function Footer() {
   return (
-    <div>
-      <Logo />
-    </div>
-  )
+    <Box as="footer" pt={2}>
+      <Container maxW="container.lg" pb={4} pt={2} borderTop="1px solid black">
+        <Grid templateColumns="repeat(3, 1fr)" gap={4}>
+          <Text fontSize="sm">{currentYear} Privacy - Terms</Text>
+
+          <Flex justifyContent="center">
+            <Logo />
+          </Flex>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
