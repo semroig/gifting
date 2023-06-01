2e0affba001ea78b3b4b1eb9e0f5d8c4048c168f
Matias M. Monasterio <matias_monasterio@outlook.com>
2023-05-31 21:50:49 -0300

    add proptype to stepper component
    Signed-off-by: Matias M. Monasterio <matias_monasterio@outlook.com>

--- src/components/pure/stepper.jsx
+++ src/components/pure/stepper.jsx
@@ -1,29 +1,37 @@
+import PropTypes from "prop-types";
 import { useEffect, useMemo } from "react";
 import { useSteps } from "@chakra-ui/react";
-import { Stepper as ChakraStepper, Step, StepIndicator, StepStatus, StepIcon, Progress, Box } from "@chakra-ui/react";
+import {
+  Stepper as ChakraStepper,
+  Step,
+  StepIndicator,
+  StepStatus,
+  StepIcon,
+  Progress,
+  Box,
+} from "@chakra-ui/react";
 
-export default function Stepper({step, max, ...args}) {
-    const { activeStep, setActiveStep } = useSteps({
-        index: 1,
-        count: max,
-    })
-    
-    const progressPercent = useMemo(() => {
-      return (activeStep) * 100 / (max - 1)
-    }, [activeStep, max])
+export default function Stepper({ step, max, ...args }) {
+  const { activeStep, setActiveStep } = useSteps({
+    index: 1,
+    count: max,
+  });
 
-    useEffect(() => {
-        setActiveStep(step)
-    }, [step])
+  const progressPercent = useMemo(() => {
+    return (activeStep * 100) / (max - 1);
+  }, [activeStep, max]);
 
-    return (
-      <Box {...args}>
-        <Box position='relative'>
+  useEffect(() => {
+    setActiveStep(step);
+  }, [step]);
 
-        <ChakraStepper size='sm' index={activeStep} gap='0'>
+  return (
+    <Box {...args}>
+      <Box position="relative">
+        <ChakraStepper size="sm" index={activeStep} gap="0">
           {new Array(max).fill(undefined).map((_, index) => (
-            <Step key={index} gap='0'>
-              <StepIndicator bg='white'>
+            <Step key={index} gap="0">
+              <StepIndicator bg="white">
                 <StepStatus complete={<StepIcon />} />
               </StepIndicator>
             </Step>
@@ -32,13 +40,18 @@ export default function Stepper({step, max, ...args}) {
 
         <Progress
           value={progressPercent}
-          position='absolute'
-          height='3px'
-          width='full'
-          top='10px'
+          position="absolute"
+          height="3px"
+          width="full"
+          top="10px"
           zIndex={-1}
         />
       </Box>
-      </Box>
-      )
+    </Box>
+  );
 }
+
+Stepper.propTypes = {
+  step: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+};
