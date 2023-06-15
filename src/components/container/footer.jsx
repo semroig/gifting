6fc82c77425598bf12e0d3f7f21f502c73d7facc
Sem Gabriel Roig <sem.roig@gmail.com>
2023-06-15 19:16:22 -0300

    add footer and color to logo

--- src/components/container/footer.jsx
+++ src/components/container/footer.jsx
@@ -8,10 +8,16 @@ export default function Footer() {
     <Box as="footer" pt={2}>
       <Container maxW="container.lg" pb={4} pt={2} borderTop="1px solid black">
         <Grid templateColumns="repeat(3, 1fr)" gap={4}>
-          <Text fontSize="sm">{currentYear} Privacy - Terms</Text>
+          <Text fontSize="sm">
+            {currentYear} - Developed by{" "}
+            <a href="https://www.linkedin.com/in/sem-gabriel-roig/">Sem</a> and{" "}
+            <a href="https://www.linkedin.com/in/matiasmonasterio/">Monas</a>
+          </Text>
 
           <Flex justifyContent="center">
-            <Logo />
+            <Text fontSize="2xl" color="accent">
+              gifting
+            </Text>
           </Flex>
         </Grid>
       </Container>
--- src/components/pure/logo.jsx
+++ src/components/pure/logo.jsx
@@ -1,3 +1,21 @@
+import {
+  Container,
+  Card,
+  CardBody,
+  Heading,
+  Text,
+  CardHeader,
+  Button,
+  Stack,
+  Image,
+  Box,
+  Center,
+} from "@chakra-ui/react";
+
 export default function Logo() {
-  return <div>gifting</div>;
+  return (
+    <Text fontSize="4xl" color="accent">
+      gifting
+    </Text>
+  );
 }
